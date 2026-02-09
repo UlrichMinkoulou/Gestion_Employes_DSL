62d465f30b8aa89077a01ac40d9ceb606756bed6
UlrichMinkoulou <ulrichsolutions97@gmail.com>
2026-02-09 02:18:49 +0100

    version 0.0 du Logiciel juste avec les connexions sans toutes les fonctionnalites

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,12 @@
 int main(void) 
 {
 
-    DataBase base_de_donne("entreprise.db");
-    Admin_db bd_admin("dataBase_admin.db");
+    // DataBase base_de_donne("entreprise.db");
+    // Admin_db bd_admin("dataBase_admin.db");
 
-    // bd_admin.ajouterAdmin();
-    bd_admin.afficherAdmin();
-    
-    // base_de_donne.ajouterEmploye();
-    base_de_donne.afficherEmploye();
+
     pageAcceuil();
-    // base_de_donne.rechercherUnEmploye();
-    // base_de_donne.changerInfoEmploye();
-    // base_de_donne.connexionEmploye();
+
 
 }
 
